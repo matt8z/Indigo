916443ff42b1bf389b262c1c4df19c92e6803b27
ebwinters <Ethan@Ethan-Winters-Computer-2.local>
2015-10-08 08:29:38 -0500

    models updated

--- HPBooks/registration/models.py
+++ HPBooks/registration/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	is_active = True
+
+	def __str__(self):
+		self.user.username
